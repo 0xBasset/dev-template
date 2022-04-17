d624c99073715630dc0b3dc2e156fa27068a8359
JG Carvalho <0xbasset@gmail.com>
2022-04-17 17:54:50 -0300

    removed ds-test dependency

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.7;
 
-import "../modules/ds-test/src/test.sol";
+import "./utils/test.sol";
+import "./utils/Interfaces.sol";
 
 contract ContractTest is DSTest {
+        
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
     function setUp() public {}
 
     function testExample() public {
